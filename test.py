b187bce329c0eb97f3c84a95802f58f8de489e58
linuxer77 <harshitg187@gmail.com>
2025-05-02 19:51:56 +0530

    ul

--- test.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Get the absolute path of the current script
-current_dir = os.path.dirname(os.path.abspath(__file__))
-print(current_dir)
-# Now, you can build the path to 'main.py' relative to the current directory
-file_path = os.path.join(current_dir, 'test.py')
-print(file_path)
-print(file_path)
